97b1952960a25420d111551f3c0a2cc5422f501b
Eric Sexton <foopis23@gmail.com>
2022-06-25 11:43:39 -0400

    Updated To New Input System

--- Assets/Scripts/Menu/SettingsMenuController.cs
+++ Assets/Scripts/Menu/SettingsMenuController.cs
@@ -32,6 +32,6 @@ public class SettingsMenuController : IRouterController
 	public override void OnRouteEnter()
 	{
         audioSlider.value = PlayerPrefs.GetFloat("volume", 1f);
-        sensitivitySlider.value = PlayerPrefs.GetFloat("sensitivity", 50f);
+        sensitivitySlider.value = PlayerPrefs.GetFloat("sensitivity", 7f);
 	}
 }
